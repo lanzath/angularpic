ab5ca4e786a046839adb119bddcd7e9e3d0179ac
Thiago Lanza <lanzathm@gmail.com>
2021-05-14 14:08:25 -0300

    App main module: ngFor directive example

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    photos = [
+        {
+            url: 'https://img.wattpad.com/cover/192306297-256-k724565.jpg',
+            description: 'anai'
+        },
+        {
+            url: 'https://ami.animecharactersdatabase.com/uploads/chars/67712-27142636.png',
+            description: 'resasuke'
+        }
+    ];
+
 }
