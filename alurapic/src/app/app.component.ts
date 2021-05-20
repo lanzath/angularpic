9e99c7a5d5b8152451d42e89033d8c08a8e1439b
Thiago Lanza <lanzathm@gmail.com>
2021-05-20 16:12:40 -0300

    Components navigation routes, not found page

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,24 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
-import { Photo } from './photos/photo/IPhoto';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-    photos: Photo[] = [];
-
-    constructor(private photoService: PhotoService) {
-
-    }
-
-    ngOnInit() {
-
-        this.photoService
-            .listFromUser('lanza')
-            .subscribe(photos => this.photos = photos);
-    }
+    constructor() {}
 }
--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
