54552686e0efd0f7cf0fa83a6e1ebc86f48371b7
Thiago Lanza <lanzathm@gmail.com>
2021-06-07 17:02:01 -0300

    Header component

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -7,21 +7,22 @@ import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
-
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ErrorsModule,
-    HomeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        PhotosModule,
+        HttpClientModule,
+        AppRoutingModule,
+        ErrorsModule,
+        HomeModule,
+        CoreModule
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
