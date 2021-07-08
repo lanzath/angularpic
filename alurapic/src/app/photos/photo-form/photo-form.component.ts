1b5883a107d66355dfbfaa1dc3562ea565eb25e0
Thiago Lanza <lanzathm@gmail.com>
2021-07-08 17:57:53 -0300

    Alert message feature

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { AlertService } from '../../shared/components/alert/alert.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
     selector: 'app-photo-form',
@@ -17,7 +19,9 @@ export class PhotoFormComponent implements OnInit {
     constructor(
         private formBuilder: FormBuilder,
         private photoService: PhotoService,
-        private router: Router
+        private router: Router,
+        private alertService: AlertService,
+        private userService: UserService
     ) { }
 
     ngOnInit() {
@@ -42,6 +46,10 @@ export class PhotoFormComponent implements OnInit {
         const allowComments = this.photoForm.get('allowComments').value;
         this.photoService
             .upload(description, allowComments, this.file)
-            .subscribe(() => this.router.navigate(['']), err => console.log(err));
+            .subscribe(
+                () => {
+                    this.alertService.success('Upload complete', true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
+                });
     }
 }
