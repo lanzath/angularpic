3b81d706448f729915365984594f6c07ef2b6978
Thiago Lanza <lanzathm@gmail.com>
2021-08-10 19:30:22 -0300

    Side menu component and behavior

--- alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -6,6 +6,8 @@ import { UserService } from '../../../core/user/user.service';
 })
 export class ShowIfLoggedDirective implements OnInit {
 
+    currentDisplay: string;
+
     constructor(
         private element: ElementRef<any>,
         private renderer: Renderer,
@@ -13,7 +15,19 @@ export class ShowIfLoggedDirective implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        !this.userService.isLogged()
-            && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+        // Ao ser carregada a diretiva pega o valor do display atual.
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+
+        // Aguarda à emissão do observable
+        this.userService.getUser().subscribe(user => {
+            if (user) {
+                // Se o usuário estiver logado então é mostrado o currentDisplay, o estado atual que o elemento tinha.
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', this.currentDisplay);
+            } else {
+                // Ao fazer logout, armazena o display atual e o substitui por none.
+                this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            }
+        });
     }
 }
